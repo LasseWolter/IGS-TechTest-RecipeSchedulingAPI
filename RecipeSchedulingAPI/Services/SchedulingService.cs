81d8d772a2d5f190812c06f4f2216c223f524317
Lasse Wolter <lasse.wolter@clevermed.com>
2024-05-04 22:00:16 +0100

    (SchedulingService)feat: Add logic to calculate lighting events + order all events
    * order all events by DateTime before returning

--- RecipeSchedulingAPI/Services/SchedulingService.cs
+++ RecipeSchedulingAPI/Services/SchedulingService.cs
@@ -21,17 +21,18 @@ public class SchedulingService : ISchedulingService
             _logger.LogError("StartDate is null, can't create schedule.");
             return null;
         }
+
         DateTime startDate = (DateTime)request.StartDate;
-        
+
         var matchedRecipes = recipes.FirstOrDefault(r => r.Name == request.RecipeName);
         if (matchedRecipes == null)
         {
             _logger.LogWarning($"No matching recipe found for RecipeRequest: {request.RecipeName}");
             return null;
         }
-        
+
         Schedule schedule = new Schedule();
-        
+
         foreach (WateringPhase wateringPhase in matchedRecipes.WateringPhases)
         {
             for (int i = 0; i < wateringPhase.Repetitions; i++)
@@ -40,6 +41,21 @@ public class SchedulingService : ISchedulingService
                 schedule.Events.Add(new Event(startDate.AddMinutes(timeOffsetMinutes), CommandType.Water, waterAmount: wateringPhase.Amount));
             }
         }
+
+        foreach (LightingPhase lightingPhase in matchedRecipes.LightingPhases)
+        {
+            for (int i = 0; i < lightingPhase.Repetitions; i++)
+            {
+                int phaseStartOffsetMinutes = lightingPhase.Hours * 60 * i + lightingPhase.Minutes * i;
+                foreach (Operation operation in lightingPhase.Operations)
+                {
+                    int operationOffsetInMinutes = phaseStartOffsetMinutes + operation.OffsetHourse * 60 + operation.OffsetMinutes;
+                    schedule.Events.Add(new Event(startDate.AddMinutes(operationOffsetInMinutes), CommandType.Light, lightIntesnity: operation.LightIntensity));
+                }
+            }
+        }
+
+        schedule.Events = schedule.Events.OrderBy(e => e.DateTimeUtc).ToList();
         return schedule;
     }
 }

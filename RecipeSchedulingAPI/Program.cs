37b6a3d99972d7b2c4be8d4d75244289576bf352
Lasse Wolter <lasse.wolter@clevermed.com>
2024-05-06 20:01:50 +0100

    feat: Add Docker and docker-compose files

--- RecipeSchedulingAPI/Program.cs
+++ RecipeSchedulingAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // TODO: Add DI
 builder.Services.AddControllers();
 builder.Logging.AddConsole();
@@ -14,11 +15,10 @@ builder.Services.AddScoped<SchedulingService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// REMARK: In most cases you wouldn't want to enable swagger in production. It would leak details about your API that you might want to keep private. 
+// Here, I'm enabling it without an environment flag to make it easy to review/explore my API and also make it part of my docker image which is built in release mode.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Handle unhandled exceptions
 
